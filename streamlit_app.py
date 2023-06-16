a24e7516796180d4c7c5da9636912fd7902b9017
Josh Gelber <106623763+JMGELBER@users.noreply.github.com>
2023-06-16 11:49:32 -0400

    Update streamlit_app.py
    added fruit choice variable into output

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # Normalize semi-structured JSON data into a flat table.
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
